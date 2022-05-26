a2e3e4d89eb81c55cb277a0e18c9fee0722b998b
unknown <cataniafrancoar@gmail.com>
2022-05-26 11:45:35 -0300

    Ahorcado terminado

--- src/app/components/page/ahorcado/ahorcado.component.ts
+++ src/app/components/page/ahorcado/ahorcado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScoresService } from 'src/app/services/scores.service';
 
 @Component({
   selector: 'app-ahorcado',
@@ -7,42 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhorcadoComponent implements OnInit {
 
-  palabra:string = '';
-
-  arrayOriginal:Array<string> = [];
-  arrayAdivinado:Array<string> = [];
-  arrayActual:Array<string> = [];
-
-  letras:Array<string> = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+  LETRAS:Array<string> = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
                           'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 
                           'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  // letra:string = '';
 
-  vidas:number = 7;
-  win:boolean = false;
+  PALABRAS:Array<string> = [
+    'SEÑUELO', 'ILUSIONES', 'PANTALLA', 'DESPEDIDA', 'FERROCARRIL', 'MANZANA', 'PERGAMINO',
+    'INVITADO', 'SALUD', 'TRABAJO', 'PAN', 'FLORES', 'CIRCO', 'BOHEMIO', 'GIRASOLES', 'MENDIGO'
+  ];
 
-  constructor() { }
+  palabra:string = '';
+  arrayOriginal:Array<string> = [];
+  arrayActual:Array<string> = [];
+  letras:Array<string> = [];
+  vidas:number = 0;
+  juegoTerminado:boolean = false;
+  puntaje:number = 0;
+
+  constructor(private scoresService:ScoresService) { }
 
   ngOnInit(): void {
-    this.palabra = "H";
-    this.arrayOriginal = this.palabra.split("");
-    this.arrayActual.length = this.arrayOriginal.length;
+    this.reiniciar();
   }
 
-  // comparar() {
-  //   this.arrayAdivinado = this.arrayOriginal.filter(
-  //     elemento => {
-  //       return elemento === this.letra
-  //     }
-  //   )
-  // }
+  reiniciar() {
+    const indice = this.getRndInteger(0, this.PALABRAS.length - 1);
+    this.palabra = this.PALABRAS[indice];
+    this.arrayOriginal = this.palabra.split("");
+    this.arrayActual = [];
+    this.arrayActual.length = this.arrayOriginal.length;
+    this.letras = [...this.LETRAS];
+    this.vidas = 10;
+    this.juegoTerminado = false;
+  }
 
-  // compararDetalle(texto:string) {
-
-  // }
+  getRndInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 
   onTeclaClick(caracter:string) {
-    // this.letra = caracter;
     let adivino:boolean = false;
 
     for (let i = 0; i < this.arrayOriginal.length; i++) {
@@ -51,8 +55,6 @@ export class AhorcadoComponent implements OnInit {
         adivino = true;
       }
     }
-
-    // this.letras = this.letras.filter(letra => letra !== caracter);
 
     this.letras = this.letras.map(
       letra => {
@@ -68,14 +70,19 @@ export class AhorcadoComponent implements OnInit {
 
     if (!adivino) {
       this.vidas--;
+      if (this.vidas === 0) {
+        this.juegoTerminado = true;
+        this.puntaje--;
+      }
     }
     else if (this.arrayActual.join("") === this.palabra) {
-      this.win = true;
+      this.juegoTerminado = true;
+      this.puntaje++;
     }
   }
-
-  onReiniciarClick() {
-
+  
+  sendScore () {
+    this.scoresService.setScore('ahorcado', this.puntaje);
   }
 
 }
