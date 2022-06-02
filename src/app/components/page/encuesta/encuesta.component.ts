1ee08bbd48cb666011527e8a9295ce284a66137b
unknown <cataniafrancoar@gmail.com>
2022-06-02 12:35:49 -0300

    userData y empezando gameData

--- src/app/components/page/encuesta/encuesta.component.ts
+++ src/app/components/page/encuesta/encuesta.component.ts
@@ -7,20 +7,29 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./encuesta.component.scss']
 })
 export class EncuestaComponent implements OnInit {
-  genders = ['male', 'female'];
-  signupForm: FormGroup;
+  juegos = [
+    'Ahorcado',
+    'Mayor o Menor',
+    'Preguntados',
+    'Alcoholímetro'
+  ];
+
+  encuestaForm: FormGroup;
 
   constructor() {
-    this.signupForm = new FormGroup({
+    this.encuestaForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, [Validators.required]),
+        'nombre': new FormControl(null, [Validators.required]),
+        'apellido': new FormControl(null, [Validators.required]),
         'edad': new FormControl(null, [Validators.required, Validators.min(18), Validators.max(99)]),
         'tel': new FormControl(null, [Validators.required, Validators.min(18), Validators.max(99)])
       }),
-      'gender': new FormControl('male')
+      'gameData': new FormGroup({
+        'juego': new FormControl(null, [Validators.required])
+      })
     });
     
-    this.signupForm.statusChanges.subscribe(
+    this.encuestaForm.statusChanges.subscribe(
       (status) => console.log(status)
     );
   }
@@ -28,11 +37,15 @@ export class EncuestaComponent implements OnInit {
   ngOnInit() {
   }
 
-  get username() { return this.signupForm.get('userData.username'); }
-  get genero() { return this.signupForm.get('gender'); }
+  get nombre() { return this.encuestaForm.get('userData.nombre'); }
+  get apellido() { return this.encuestaForm.get('userData.apellido'); }
+  get edad() { return this.encuestaForm.get('userData.edad'); }
+  get tel() { return this.encuestaForm.get('userData.tel'); }
+
+  get juego() { return this.encuestaForm.get('gameData.juego'); }
 
   onSubmit() {
-    console.log(this.signupForm);
-    this.signupForm.reset();
+    console.log(this.encuestaForm);
+    this.encuestaForm.reset();
   }
 }
