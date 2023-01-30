9e3750cf3e86b418ef31fe0de2f202d1a0619c10
unknown <cataniafrancoar@gmail.com>
2023-01-30 20:17:41 -0300

    @HostListener testing

--- src/app/components/page/bienvenida/bienvenida.component.ts
+++ src/app/components/page/bienvenida/bienvenida.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bienvenida',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bienvenida.component.scss']
 })
 export class BienvenidaComponent implements OnInit {
+  isMobile: boolean;
 
-  constructor() { }
+  constructor() {
+    this.isMobile = window.innerWidth < 768;
+  }
 
   ngOnInit(): void {
   }
 
+  @HostListener('window:resize')
+    onResize() {
+      let screenWidth = window.innerWidth;
+
+      if (screenWidth < 768) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+  }
 }
